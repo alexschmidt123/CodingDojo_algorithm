0d9562d5c5e3a293e3f64b7dd12f50fdee759007
Gaoming Lin <gaominglins@gmail.com>
2022-03-17 11:03:32 -0400

    p

--- w8d4.js
+++ w8d4.js
@@ -10,15 +10,19 @@ output: "aaabbbbcdd"
 const strDecode = (str) => {
     let currChar = str[0];
     let output = "";
+    let currNum = "";
     for (let i=0; i<str.length; i++){
         console.log(str[i]);
         if (!isNaN(str[i])){
-            output += currChar.repeat(str[i]);
+            currNum += str[i];
         }else{
+            output += currChar.repeat(currNum);
             currChar = str[i];
+            currNum = "";
         }
     }
+    output += currChar.repeat(currNum);
     return output
 }
 
-console.log(strDecode("a3b4c1d2"))
+console.log(strDecode("a13b4c1d12"))
